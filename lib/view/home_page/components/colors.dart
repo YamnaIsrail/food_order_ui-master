5f68258f2053c1228c503327f1bf6024a57e3885
yum <yamnaisrailkhan@gmail.com>
2024-05-31 18:49:31 +0500

    Food app UI

---
+++ lib/view/home_page/components/colors.dart
@@ -0,0 +1,9 @@
+import 'dart:ui';
+
+var buttonColor = Color(0xFF12476C);
+var registerColor = Color(0xFF12476C);
+var lightColor = Color(0xFFFFC971);
+var freeDelivery = Color(0xFF025939);
+var textColor = Color(0xFFf2ac29);
+var texthint = Color(0xFF666666);
+var passiveFavorite = Color(0xFFD9886A);
